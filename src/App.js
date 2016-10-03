2f0a9cf1f1221a16ece6dfbfa42f7beda90be641
Art Armstrong <arthurjarmstrong@gmail.com>
2016-10-03 15:21:58 -0700

    Start creating new ui login page.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib';
-import './App.css';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import logo from './defi-logo-color.png';
+
+const styles = {
+  paper: {
+    marginTop: '30px',
+    padding: '60px 30px ',
+  },
+  button: {
+    marginTop: '30px',
+  }
+};
 
 class App extends Component {
   render() {
     return (
-      <Grid fluid>
-        <Row>
-          <Col sm={3} md={2} lg={5} />
-          <Col sm={6} md={8} lg={2}>
-            <div className="App">
-              <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-              </div>
-              <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-              </p>
-            </div>
-          </Col>
-          <Col sm={3} md={2} lg={5} />
-        </Row>
-      </Grid>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <Grid fluid>
+          <Row center="xs" middle="xs">
+            <Col xs={12} sm={8} md={5} lg={5}>
+              <Paper zDepth={3} style={styles.paper}>
+                <Row center="xs">
+                  <Col xs={12}>
+                    <img src={logo} alt="logo" />
+                  </Col>
+                  <Col xs={12}>
+                    <TextField
+                      fullWidth="true"
+                      floatingLabelText="Username"
+                    />
+                  </Col>
+                  <Col xs={12}>
+                    <TextField
+                      fullWidth="true"
+                      floatingLabelText="Password"
+                      type="password"
+                    />
+                  </Col>
+                  <Col xs={12}>
+                    <RaisedButton
+                      style={styles.button}
+                      primary={true}
+                      label="Login"
+                    />
+                  </Col>
+                </Row>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
