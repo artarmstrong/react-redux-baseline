0b0456bfe975db8b9091390e9fe85ac0411593f8
Art Armstrong <arthurjarmstrong@gmail.com>
2016-10-04 15:41:42 -0700

    Update some validation and error output

--- src/App.js
+++ src/App.js
@@ -23,58 +23,120 @@ const styles = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var username = this.state.username.trim();
+    var password = this.state.password.trim();
+    if (!username || !password) {
+      if (!username) {
+        this.setState({ errorUsername: 'Username required' });
+      } else {
+        this.setState({ errorUsername: '' });
+      }
+
+      if (!password) {
+        this.setState({ errorPassword: 'Password required' });
+      } else {
+        this.setState({ errorPassword: '' });
+      }
+
+      return;
+    } else {
+      alert('Username: ' + username + ' / Password: ' + password);
+    }
+
+    this.setState({
+      username: '',
+      password: '',
+      errorUsername: '',
+      errorPassword: '',
+    });
+  }
+
+  handleUsernameChange(e) {
+    this.setState({ username: e.target.value });
+    if (this.state.errorUsername) {
+      this.setState({ errorUsername: '' });
+    }
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+    if (this.state.errorPassword) {
+      this.setState({ errorPassword: '' });
+    }
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-        <Grid fluid>
+        <Grid>
           <Row center="xs" middle="xs">
-            <Col xs={12} sm={8} md={6} lg={6}>
+            <Col xs={10} sm={6} md={4} lg={4}>
               <Paper zDepth={3} style={styles.paper}>
-                <Row center="xs">
-                  <Col xs={12}>
-                    <img src={headerLogo} alt="logo" />
-                  </Col>
-                  <Col xs={12}>
-                    <TextField
-                      fullWidth="true"
-                      floatingLabelText="Username"
-                    />
-                  </Col>
-                  <Col xs={12}>
-                    <TextField
-                      fullWidth="true"
-                      floatingLabelText="Password"
-                      type="password"
-                    />
-                  </Col>
-                  <Col xs={12}>
-                    <RaisedButton
-                      style={styles.button}
-                      primary={true}
-                      label="Login"
-                    />
-                  </Col>
-                  <Col xs={8}>
-                    <p>For help please don't call 214-732-9199</p>
-                  </Col>
-                  <Col xs={8}>
-                    <p>
-                      <small>
-                        <strong>defi</strong>&nbsp;
-                        deployed&nbsp;
-                        <strong>SOW-867-Adding-CUDL-Forwarding-Support</strong>&nbsp;
-                        on 10/3/2016 1:30:27 PM
-                      </small>
-                    </p>
-                  </Col>
-                  <Col xs={8}>
-                    <a
-                    href="https://kraken.defisolutions.com/app#/projects/definitive/releases/create"
-                    class="btn btn-danger btn-sm">
-                      Deploy
-                    </a>
-                  </Col>
-                </Row>
+                <form action="#" method="POST" onSubmit={this.handleSubmit}>
+                  <Row center="xs">
+                    <Col xs={12}>
+                      <img src={headerLogo} alt="logo" />
+                    </Col>
+                    <Col xs={12}>
+                      <TextField
+                        fullWidth={true}
+                        floatingLabelText="Username"
+                        value={this.state.username}
+                        errorText={this.state.errorUsername}
+                        onChange={this.handleUsernameChange.bind(this)}
+                      />
+                    </Col>
+                    <Col xs={12}>
+                      <TextField
+                        fullWidth={true}
+                        floatingLabelText="Password"
+                        value={this.state.password}
+                        errorText={this.state.errorPassword}
+                        onChange={this.handlePasswordChange.bind(this)}
+                        type="password"
+                      />
+                    </Col>
+                    <Col xs={12}>
+                      <RaisedButton
+                        type="submit"
+                        style={styles.button}
+                        primary={true}
+                        label="Login"
+                      />
+                    </Col>
+                    <Col xs={8}>
+                      <p>For help please don't call 214-732-9199</p>
+                    </Col>
+                    <Col xs={8}>
+                      <p>
+                        <small>
+                          <strong>defi</strong>&nbsp;
+                          deployed&nbsp;
+                          <strong>SOW-867-Adding-CUDL-Forwarding-Support</strong>&nbsp;
+                          on 10/3/2016 1:30:27 PM
+                        </small>
+                      </p>
+                    </Col>
+                    <Col xs={8}>
+                      <a
+                      href="https://kraken.defisolutions.com/app#/projects/definitive/releases/create"
+                      className="btn btn-danger btn-sm">
+                        Deploy
+                      </a>
+                    </Col>
+                  </Row>
+                </form>
               </Paper>
               <a href="http://defisolutions.com" target="_blank">
                 <img src={poweredLogo} style={styles.bottom} alt="logo" />
